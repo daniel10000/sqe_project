9a8eccfef4bea1bb6d53eccbd45eb4a3472793b1
daniel <daniel@localhost>
2018-01-11 10:24:12 +0100

    added controller

---
+++ ElevatorControlCenter/src/main/java/at/fhhagenberg/sqe/controller/ElevatorController.java
@@ -0,0 +1,13 @@
+package at.fhhagenberg.sqe.controller;
+
+import javafx.beans.property.IntegerProperty;
+
+public class ElevatorController
+{
+	
+	private IntegerProperty counter;
+	
+	public IntegerProperty counterProperty() {
+	    return counter;
+	}
+}
